b40dce6be1c49d5cf8fbac16e8081d01b9e4029b
PopoviciSerban <serbangaming24@gmail.com>
2020-04-24 02:47:29 +0300

    Add project files.

---
+++ ComplexD_Numbers/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplexD_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    internal class Complex
+    {
+        private int real;
+        private int imaginary;
+
+        public Complex() : this(0)
+        {
+        }
+
+        public Complex(int real_part)
+        {
+            real = real_part;
+            imaginary = 0;
+        }
+
+        public Complex(int real_part, int imaginary_part)
+        {
+            real = real_part;
+            imaginary = imaginary_part;
+        }
+
+        public static Complex operator +(Complex c) => c;
+        public static Complex operator -(Complex c) => new Complex(-c.real, -c.imaginary);
+
+        public static Complex operator +(Complex a, Complex b) => a + b;
+
+        public static Complex operator -(Complex a, Complex b) => a + (-b);
+
+        public static Complex operator *(Complex a, Complex b)
+            => new Complex(a.real * b.real - a.imaginary * b.imaginary, a.real * b.imaginary + a.imaginary * b.real);
+
+        public static Complex operator ^(Complex a, int b)
+        {
+            Complex x = a;
+
+            for (int i = 2; i <= b; i++)
+                x *= a;
+
+            return x;
+        }
+
+        public void Trigonometric_PowerUp(int b)
+        {
+            double r = Math.Sqrt(real * real + imaginary * imaginary);
+            double teta = Math.Atan(imaginary / real);
+
+            double aux = r;
+
+            for (int i = 2; i <= b; i++)
+                r *= aux;
+
+            Console.WriteLine("The trigonometric form of the complex number powered up is {0} * ({1} + i * {2})", r, Math.Cos(b * teta), Math.Sin(b * teta));
+        }
+
+        public void Trigonometric_Form()
+        {
+            double r = Math.Sqrt(real * real + imaginary * imaginary);
+            double teta = Math.Atan(imaginary / real);
+
+            Console.WriteLine("The trigonometric form of the complex number is {0} * ({1} + i * {2})", r, Math.Cos(teta), Math.Sin(teta));
+        }
+
+    }
+}
