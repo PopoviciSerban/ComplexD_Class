3f0ae15251ccc5e8575db1fa9fa1c12b43fc063b
PopoviciSerban <57687392+PopoviciSerban@users.noreply.github.com>
2020-04-24 16:04:53 +0300

    Update Program.cs

--- ComplexD_Numbers/Program.cs
+++ ComplexD_Numbers/Program.cs
@@ -1,15 +1,40 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ComplexD_Numbers
+namespace NumereComplexe
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Complex a = new Complex(3, 2);
+            Complex b = new Complex(2, 1);
+            Complex c;
+
+            Console.WriteLine("a = {0}", a);
+            Console.WriteLine("b = {0}", b);
+
+            c = a + b;
+            Console.WriteLine("a + b = {0}", c);
+
+            c = a - b;
+            Console.WriteLine("a - b = {0}", c);
+
+            c = a * b;
+            Console.WriteLine("a * b = {0}", c);
+
+            Console.Write("Give the power value: ");
+            int power = int.Parse(Console.ReadLine());
+            c = a ^ power;
+            Console.WriteLine("a ^ {0} = {1}", power, c);
+            
+            a.Trigonometric_PowerUp(power);
+            a.Trigonometric_Form();
+
+            Console.ReadKey();
         }
     }
 
@@ -37,7 +62,8 @@ namespace ComplexD_Numbers
         public static Complex operator +(Complex c) => c;
         public static Complex operator -(Complex c) => new Complex(-c.real, -c.imaginary);
 
-        public static Complex operator +(Complex a, Complex b) => a + b;
+        public static Complex operator +(Complex a, Complex b)
+            => new Complex(a.real + b.real, a.imaginary + b.imaginary);
 
         public static Complex operator -(Complex a, Complex b) => a + (-b);
 
@@ -49,11 +75,12 @@ namespace ComplexD_Numbers
             Complex x = a;
 
             for (int i = 2; i <= b; i++)
+
                 x *= a;
 
             return x;
         }
-
+        
         public void Trigonometric_PowerUp(int b)
         {
             double r = Math.Sqrt(real * real + imaginary * imaginary);
@@ -72,8 +99,9 @@ namespace ComplexD_Numbers
             double r = Math.Sqrt(real * real + imaginary * imaginary);
             double teta = Math.Atan(imaginary / real);
 
-            Console.WriteLine("The trigonometric form of the complex number is {0} * ({1} + i * {2})", r, Math.Cos(teta), Math.Sin(teta));
+            Console.WriteLine("Trigonometric form of the complex number is {0} * ({1} + i * {2})", r, Math.Cos(teta), Math.Sin(teta));
         }
 
+        public override string ToString() => $"{real} + {imaginary} * i";
     }
 }
